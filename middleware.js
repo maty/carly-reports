10c37097242cf6df0953b192bd1f2252f74c41fc
Matias Puga <matiaspuga@gmail.com>
2026-08-07 10:52:21 +0200

    deploy

--- middleware.js
+++ middleware.js
@@ -25,5 +25,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|go).*)'],
 };
